4baba35b9f19725f660d92b84dce79bcede32c15
Lauri Maila <laurikmaila@gmail.com>
2023-08-13 16:24:17 +0300

    Completed 4.5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,6 +10,25 @@ const totalLikes = (blogs) => {
     )
 }
 
+const favouriteBlog = (blogs) => {
+
+    if (blogs === undefined || blogs.length == 0) {
+        return null
+    }
+
+    const favourite = blogs.reduce(
+        (prev, currentBlog) => {
+            return (prev.likes > currentBlog.likes) ? prev : currentBlog
+        }
+    )
+
+    delete favourite._id;
+    delete favourite.url;
+    delete favourite.__v;
+
+    return favourite
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favouriteBlog
 }
